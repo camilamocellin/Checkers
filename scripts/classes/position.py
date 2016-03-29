5fd68efdee2f037ab45cd4f8ab3cdb70c026936f
LuizIFC <luiz.allievi@inf.concordia.ifc.edu.br>
2016-03-29 10:23:00 -0300

    commit 1, some classes was defined and position classe was started

--- scripts/classes/position.py
+++ scripts/classes/position.py
@@ -2,16 +2,20 @@ class Position(object):
 
     def __init__(self):
         self.occupation = []
-        self.near = [
-            (self.position[0] - 1, self.position[1] - 1),
-            (self.position[0] - 1, self.position[1] + 1),
-            (self.position[0] + 1, self.position[1] - 1),
-            (self.position[0] + 1, self.position[1] + 1),
-        ]
+
         self.position = (3,3)
 
         def get_near(self):
             return self.near
+
+        def set_near(self):
+            #will be added some verifications before this
+            self.near = [
+                (self.position[0] - 1, self.position[1] - 1),
+                (self.position[0] - 1, self.position[1] + 1),
+                (self.position[0] + 1, self.position[1] - 1),
+                (self.position[0] + 1, self.position[1] + 1),
+            ]
 
 
 
